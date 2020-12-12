5c518537636c6cac33d00ef0f29d9da52d155ed2
samuelOsborne <samu.osborne@gmail.com>
2020-12-12 14:40:41 +0100

    Changed output dir

--- serve-examples.js
+++ serve-examples.js
@@ -6,8 +6,7 @@ const open = require('open');
 const app = express();
 
 app.use(serveStatic('examples', {'index': ['index.html', 'index.htm']}));
-app.use('/bin', express.static(path.join(__dirname, 'bin')));
-// app.use('/bin', express.static(path.join(__dirname, 'bin')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use('/src', express.static(path.join(__dirname, 'src')));
 app.listen(3000);
